4c86f2d3775bec69285d4d443655da1eea45cf77
Curtis Wendel <cwendel16834@gmail.com>
2019-12-22 08:45:22 -0600

    Initial commit

---
+++ src/main/kotlin/com/example/graphqlbugrepro/GraphqlbugreproApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlbugrepro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlbugreproApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlbugreproApplication>(*args)
+}
